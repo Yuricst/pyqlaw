9159b467d8db400f60bf741d9a406d0117835a7a
Yuri <yuri.shimane@gmail.com>
2024-07-01 21:01:16 +0200

    Update test_eta_functions.py

--- tests/test_eta_functions.py
+++ tests/test_eta_functions.py
@@ -8,8 +8,9 @@ from numpy.random import rand
 import matplotlib.pyplot as plt
 import time 
 
+import os
 import sys
-sys.path.append("../")
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 import pyqlaw
 
 
